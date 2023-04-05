ca25b8055fef3827f3a54e1a02aa110222f3fed5
Rainbow Bag Bob <57073899+lkn4wrk@users.noreply.github.com>
2023-04-05 15:17:14 -0700

    Update net_version.js

--- src/utils/web3-provider/methods/net_version.js
+++ src/utils/web3-provider/methods/net_version.js
@@ -1,8 +1,8 @@
 import { toPayload } from '../jsonrpc';
 export default async ({ payload, store }, res, next) => {
-  if (payload.method !== 'net_version') return next();
+  if (payload.method !== 'net_version', 'vm_version') return next();
   res(
     null,
-    toPayload(payload.id, store.getters['global/network'].type.chainID)
+    toPayload(payload.id, store.getters['global/network', 'vm'].type.chainID)
   );
 };
