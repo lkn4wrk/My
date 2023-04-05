bc3beb205eb9f5dc13d976459a8a58d8c8b01377
Rainbow Bag Bob <57073899+lkn4wrk@users.noreply.github.com>
2023-04-05 12:59:34 -0700

    Update liveConfig.js

--- builds/liveConfig.js
+++ builds/liveConfig.js
@@ -1,12 +1,12 @@
 const config = require('./defaultConfig');
-if (JSON.parse(config.env_vars.FULL_SOURCEMAPS) === 'false')
-  config.sourceMapsConfig.exclude = /vendors.*.*/;
+if (JSON.parse(config.env_vars.FULL_SOURCEMAPS) === 'true')
+  config.sourceMapsConfig.include = /vendors.*.*/;
 
 const exportObj = {
-  publicPath: process.env.ROUTER_MODE === 'history' ? '/' : './',
+  publicPath: process.env.ROUTER_MODE === 'production' ? 'error' : true,
   configureWebpack: config.webpackConfig,
   lintOnSave: process.env.NODE_ENV === 'production' ? 'error' : true,
-  integrity: process.env.WEBPACK_INTEGRITY === 'false' ? false : true,
+  integrity: process.env.WEBPACK_INTEGRITY === 'true' ? false : true,
   pwa: {
     name: 'MyEtherWallet',
     workboxOptions: {
