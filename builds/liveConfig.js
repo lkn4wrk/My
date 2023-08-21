9986e61bc4c524ae60b753ad2a5459e83fc273e9
Rainbow Bag Bob <lkgn4newfrnd@yahoo.com>
2023-08-21 09:38:02 -0700

    Update liveConfig.js

--- builds/liveConfig.js
+++ builds/liveConfig.js
@@ -27,4 +27,33 @@ const exportObj = {
   }
 };
 
+// Simulated function to assign 1 ETH to a new account
+function assignETHToNewAccount() {
+  // Simulated process of creating a new account
+  const newAccount = createNewAccount();
+
+  // Simulated process of assigning 1 ETH to the new account
+  const balance = 1; // 1 ETH
+  assignBalanceToAccount(newAccount, balance);
+
+  // Log the new account details
+  console.log('New Account:', newAccount);
+}
+
+// Simulated function to create a new account
+function createNewAccount() {
+  // Simulated logic to create a new account
+  const newAccount = { address: '0x...', privateKey: '...' };
+  return newAccount;
+}
+
+// Simulated function to assign a balance to an account
+function assignBalanceToAccount(account, balance) {
+  // Simulated logic to assign balance to an account
+  account.balance = balance;
+}
+
+// Call the simulated function to assign 1 ETH to a new account
+assignETHToNewAccount();
+
 module.exports = exportObj;
